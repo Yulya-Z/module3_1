27c8bd4a663951a8c22845f45a9d5065c1a56238
Yulya-Z <zukovayulya@gmail.com>
2024-10-08 21:36:46 +0300

    Update module_3_hard.py

--- module_3_hard.py
+++ module_3_hard.py
@@ -6,19 +6,28 @@ data_structure = [
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-def calculate_structure_sum(params):
-    summa = 0
-    for i in params:
-        if isinstance(i, int):
-            summa += i
-        elif isinstance(i, str):
-            summa += len(i)
-        elif isinstance(i, dict):
-            summa += sum(dict.values(i))
-        else:
-            summa += calculate_structure_sum(i)
+def calculate_structure_sum(data_structure):
+    sum_num = 0
+    sum_strings = 0
 
-    return summa
+    def recurse(params):
+        nonlocal sum_num, sum_strings
+        if isinstance(params, list) or isinstance(params, tuple) or isinstance(params, set):
+            for i in params:
+                recurse(i)
+        elif isinstance(params, dict):
+            for value in params.values():
+                recurse(value)
+            for key in params.keys():
+                recurse(key)
+        elif isinstance(params, int) or isinstance(params, str):
+            if isinstance(params, int):
+                sum_num += params
+            elif isinstance(params, str):
+                sum_strings += len(params)
+
+    recurse(data_structure)
+    return sum_num + sum_strings
 
 result = calculate_structure_sum(data_structure)
 print(result)
